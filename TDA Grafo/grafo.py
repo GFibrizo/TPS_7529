4e780df33ae1f0d5f1cdd781ab8f53bdf11f7045
mvbattan <mvbattan@gmail.com>
2016-09-24 23:07:09 -0300

    Refactor sobre grafo.py

--- TDA Grafo/grafo.py
+++ TDA Grafo/grafo.py
@@ -7,56 +7,54 @@ class Digraph:
 
   El grafo se crea vacío, se añaden las aristas con add_edge(). Una vez
   creadas, las aristas no se pueden eliminar, pero siempre se puede añadir
-  nuevas aristas.
-  """
-  def __init__(g, V):
-    """Construye un grafo sin aristas de V vértices.
-    """
+  nuevas aristas. """
 
-  def V(g):
+  def __init__(self, V):
+    """Construye un grafo sin aristas de V vértices."""
+    self.node_count = V
+
+  def get_vertices(self):
     """Número de vértices en el grafo.
     """
+    return self.vertices
+
+  def get_edges(self):
+    """Número de aristas en el grafo."""
     pass
 
-  def E(g):
-    """Número de aristas en el grafo.
-    """
+  def get_adjacent_edges_from(self, v):
+    """Itera sobre los aristas incidentes _desde_ el vertice v."""
     pass
 
-  def adj_e(g, v):
-    """Itera sobre los aristas incidentes _desde_ v.
-    """
+  def get_adjacent_edges_to(self, v):
+    """Itera sobre los vértices adyacentes al vertice ‘v’."""
     pass
 
-  def adj(g, v):
-    """Itera sobre los vértices adyacentes a ‘v’.
-    """
+  def add_edge(self, u, v, weight=0):
+    """Añade una arista al grafo. """
     pass
 
-  def add_edge(g, u, v, weight=0):
-    """Añade una arista al grafo.
-    """
-    pass
-
-  def __iter__(g):
+  def __iter__(self):
     """Itera de 0 a V."""
-    return iter(range(g.V()))
+    return iter(range(self.get_vertices()))
 
   def iter_edges(g):
-    """Itera sobre todas las aristas del grafo.
-
-    Las aristas devueltas tienen los siguientes atributos de solo lectura:
-
-        e.src
-        e.dst
-        e.weight
-    """
+    """Itera sobre todas las aristas del grafo."""
     pass
 
 class Arista:
-  """Arista de un grafo.
-  """
+  """Arista de un grafo."""
+
   def __init__(self, src, dst, weight):
-    pass
+    self.src = src
+    self.dst = dst
+    self.weight = weight
 
+  def src(self):
+    return self.src
 
+  def dst(self):
+    return self.dst
+
+  def weight(self):
+    return self.weight
