b377c40ee405bb4db35906cc26720c1765f0f97b
Ezequiel <ezequielmreyes@gmail.com>
2016-10-05 20:55:49 -0300

    Funciones implementadas del grafo

--- TDA Grafo/grafo.py
+++ TDA Grafo/grafo.py
@@ -1,60 +1,110 @@
 # -*- coding: latin-1 -*-
 class Digraph:
-  """Grafo no dirigido con un número fijo de vértices.
+    """Grafo no dirigido con un número fijo de vértices.
 
-  Los vértices son siempre números enteros no negativos. El primer vértice
-  es 0.
+    Los vértices son siempre números enteros no negativos. El primer vértice
+    es 0.
 
-  El grafo se crea vacío, se añaden las aristas con add_edge(). Una vez
-  creadas, las aristas no se pueden eliminar, pero siempre se puede añadir
-  nuevas aristas. """
+    El grafo se crea vacío, se añaden las aristas con add_edge(). Una vez
+    creadas, las aristas no se pueden eliminar, pero siempre se puede añadir
+    nuevas aristas. """
 
-  def __init__(self, V):
-    """Construye un grafo sin aristas de V vértices."""
-    self.vertices = V
+    def __init__(self, V):
+        """Construye un grafo sin aristas de V vértices."""
+        self.vertices = V
+        self.aristas = {}
+        for x in range(0, self.vertices):
+            self.agregar(x)
 
-  def get_vertices(self):
-    """Número de vértices en el grafo.
-    """
-    return self.vertices
+    def V(self):
+        """N�mero de v�rtices en el grafo.
+           """
+        return self.vertices
 
-  def get_edges(self):
-    """Número de aristas en el grafo."""
-    pass
+    def E(self):
+       return len(self.iter_edges())
 
-  def get_adjacent_edges_from(self, v):
-    """Itera sobre los aristas incidentes _desde_ el vertice v."""
-    pass
+    def adj_e(self, v):
+        """Itera sobre los aristas incidentes _desde_ v.
+            """
+        try:
+            return self.aristas[v]
+        except:
+            print 'Vertice no valido'
 
-  def get_adjacent_edges_to(self, v):
-    """Itera sobre los vértices adyacentes al vertice ‘v’."""
-    pass
+    def adj(self, v):
+        """Itera sobre los v�rtices adyacentes a ?v?.
+           """
+        try:
+            vertices = []
+            for arista in self.aristas[v]:
+                vertices.append(arista.dst)
+            return vertices
+        except:
+            print 'Vertice no valido'
 
-  def add_edge(self, u, v, weight=0):
-    """Añade una arista al grafo. """
-    pass
+    def add_edge(self, u, v, weight=0):
+        """A�ade una arista al grafo.
+           """
+        try:
+            if v > self.V() - 1:
+                print 'Vertice no valido'
+                return
+            aristas = self.aristas[u]
+            for arista in aristas:
+                if arista.dst == v:
+                    ## si ya existe actualizo el peso
+                    arista.weight = weight
+                    return
+            self.aristas[u].append(Arista(u, v, weight))
+        except:
+            print 'Vertice no valido'
 
-  def __iter__(self):
-    """Itera de 0 a V."""
-    return iter(range(self.get_vertices()))
+    def __iter__(self):
+        """Itera de 0 a V."""
+        return iter(range(self.get_vertices()))
 
-  def iter_edges(g):
-    """Itera sobre todas las aristas del grafo."""
-    pass
+    def iter_edges(self):
+        aristas_totales = []
+        for indice in self.aristas:
+            for arista in self.aristas[indice]:
+                aristas_totales.append(arista)
+        return aristas_totales
+
+    def agregar(self, nodo):
+        self.aristas.update({nodo: []})
+
 
 class Arista:
-  """Arista de un grafo."""
+    """Arista de un grafo."""
 
-  def __init__(self, src, dst, weight):
-    self.src = src
-    self.dst = dst
-    self.weight = weight
+    def __init__(self, src, dst, weight):
+        self.src = src
+        self.dst = dst
+        self.weight = weight
 
-  def src(self):
-    return self.src
+    def src(self):
+        return self.src
 
-  def dst(self):
-    return self.dst
+    def dst(self):
+        return self.dst
 
-  def weight(self):
-    return self.weight
+    def weight(self):
+        return self.weight
+
+
+graph = Digraph(5)
+graph.add_edge(3, 2, 5)
+graph.add_edge(1, 3, 5)
+graph.add_edge(3, 3, 5)
+graph.add_edge(1, 4, 7)
+graph.add_edge(2, 4, 7)
+graph.add_edge(2, 4, 4)
+graph.add_edge(2, 1, 7)
+vertices = graph.adj(2)
+
+print('Cantidad de aristas:' + str(graph.E()))
+print('Cantidad de vertices:' + str(graph.V()))
+print('Listado de vertices:' +str(graph.adj(1)))
+print('Listado de aristas:' + str(graph.adj_e(2)))
+print('Listado de aristas total:' + str(graph.iter_edges()))
