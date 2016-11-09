680709c28f2c442cda6edf6a7e096d5a3127a62c
Ezequiel <ereyes@summasolutions.net>
2016-11-08 22:31:06 -0300

    Added algorithm to resolve knapsack problem.

---
+++ TP2/calculador_mochila.py
@@ -0,0 +1,22 @@
+def calcular_mochila(items, pesoMax):
+    matrizDevolucion = []
+    valores = [[0] * (pesoMax + 1)
+                  for i in xrange(len(items) + 1)]
+    for i, (valor, peso, id) in enumerate(items):
+        i += 1
+        for capacidad in xrange(pesoMax + 1):
+            if peso > capacidad:
+                valores[i][capacidad] = valores[i - 1][capacidad]
+            else:
+                primerCandidato = valores[i - 1][capacidad]
+                segundoCandidato = valores[i - 1][capacidad - peso] + valor
+                valores[i][capacidad] = max(primerCandidato, segundoCandidato)
+    longItems = len(items)
+    j = pesoMax
+    while longItems > 0:
+        if valores[longItems][j] != valores[longItems - 1][j]:
+            matrizDevolucion.append(items[longItems - 1])
+            j -= items[longItems - 1][1]
+        longItems -= 1
+    matrizDevolucion.reverse()
+    return valores[len(items)][pesoMax], matrizDevolucion
--- TP2/mochila.py
+++ TP2/mochila.py
@@ -1,3 +1,7 @@
+import calculador_mochila
+import csv
+import sys
+
 CONST_NOMBRE = 0
 CONST_N = 1
 CONST_C = 2
@@ -6,21 +10,37 @@ CONST_TIEMPO = 4
 CONST_PRIMERA_LINEA = 5
 CONST_SEPARADOR = "-----\n"
 if __name__ == '__main__':
-    with open('file', 'rb') as archivo:
-        lineas = archivo.readlines()
+    if len(sys.argv) != 3:
+        print('parametro 1: archivo de entrada, parametro 2: archivo de salida')
+        sys.exit()
+    archivoEntrada = sys.argv[1]
+    archivoSalida = sys.argv[2]
+    with open(archivoSalida, 'w') as csvfile:
+        spamwriter = csv.writer(csvfile)
+        with open(archivoEntrada, 'rb') as archivo:
+                lineas = archivo.readlines()
+        cantLineas = len(lineas)
+        i = 0
+        listado = 0
+        while(i < cantLineas):
+            listado = listado + 1
+            items = []
+            pesoMaximo = int(lineas[CONST_C + i].split(" ")[1])
+            i = i + 5
+            while(lineas[i] != CONST_SEPARADOR):
+                separada = lineas[i].split(",")
+                id = int(separada[0])
+                valor = int(separada[1])
+                peso = int(separada[2])
+                items.append([valor, peso, id])
+                i = i + 1
+            mejorValor, objetos = calculador_mochila.calcular_mochila(items, pesoMaximo)
+            spamwriter.writerow(["Mejor valor para listado numero {0}: {1} y peso maximo: {2}".format(listado, mejorValor, pesoMaximo)])
+            for valor, peso, id in objetos:
+                 spamwriter.writerow(["Id:{2}, Valor: {0}, Peso: {1}".format(valor, peso, id)])
+            i = i + 2
+            spamwriter.writerow(["-------"])
 
-    cantLineas = len(lineas)
-    i = 0
-    while(i < cantLineas):
-        items = []
-        pesoMaximo = int(lineas[CONST_C + i].split(" ")[1])
-        i = i + 5
-        while(lineas[i] != CONST_SEPARADOR):
-            separada = lineas[i].split(",")
-            valor = int(separada[1])
-            peso = int(separada[2])
-            id = int(separada[0])
-            items.append([valor, peso,id])
-            i = i + 1
-        i = i + 2
+
+
 
